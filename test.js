4ea8eb64bbfe056a60f304607aae9653912bf3c5
Youenlb <79369892+Youenlb@users.noreply.github.com>
2023-03-12 22:14:39 +0100

    Add files via upload
    - Ajout de la deuxième fonction de test moreNeighbors()
    - Ajout de couleurs pour les consoles.log afin que l'affichage soit plus lisible avec des couleurs différente

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function outsideTheContinent()
+function outsideTheContinent() //Retourne les pays avec au moins un pays frontalier en dehors de son continent
 {
     let tableau_continent_different = {};
     let pays;
@@ -28,4 +28,46 @@ function outsideTheContinent()
 }
 console.log("Pays avec au moins un pays frontalier en dehors de son continent : ");
 console.log(outsideTheContinent());
+
+function moreNeighbors()
+{
+    let tableau_pays_max_voisin = {};
+    let max = 0;
+    for(let code_pays in Country.all_countries)
+    {
+        pays = Country.all_countries[code_pays];
+        pays_frontalier = pays.getBorders();
+        if(pays_frontalier !== undefined)
+        {
+            if(pays_frontalier.length > max)
+            {
+                tableau_pays_max_voisin = {};
+                tableau_pays_max_voisin[code_pays] = pays;
+                max = pays_frontalier.length;
+            }
+            else if(pays_frontalier.length === max)
+            {
+                tableau_pays_max_voisin[code_pays] = pays;
+            }
+        }
+    }
+    return tableau_pays_max_voisin;
+}
+
+console.log("Les pays avec les plus grand nombre de voisins : ");
+let tab_max_voisin = moreNeighbors();
+let i = 0;
+for(let code_pays in tab_max_voisin)
+{
+    let color = "\x1b[" + (random + 31) + "m"; // Calcule le code d'échappement de couleur pour que l'affichage sur la console soit plus lisible
+    console.log(color+"Pays : ");
+    console.log(color+tab_max_voisin[code_pays].toString());
+    console.log(color+"Voisin : ");
+    for(let code_voisin of tab_max_voisin[code_pays]["borders"])
+    {
+        console.log(color+Country.all_countries[code_voisin].toString());
+    }
+    console.log(color+"--------------------------------------------------------------------");
+    i++;
+}
 
