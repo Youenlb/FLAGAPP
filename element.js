2b91d472de14de16d2d66daede84fb759b39abaf
stgourmelon <steven.gourmelon@etudiant.univ-rennes1.fr>
2023-04-01 14:38:46 +0200

    Partie 3 terminée (à vérifier)

--- element.js
+++ element.js
@@ -5,6 +5,38 @@ const PAS_SURFACE = "Indéfini";
 const ID_TBODY_PAYS = "contenu-table-pays"
 const NB_PAYS_PAR_PAGE = 25;
 
+var data = JSON.parse("[]");
+
+let xhr = new  XMLHttpRequest ();
+if (!xhr) {
+   alert("Erreur de creation de l'objet XML HTTP Request");
+} else {
+   xhr.open("GET", "./gateway_to_restcountries.php", true);
+   xhr.onreadystatechange = process;
+   xhr.send(null);
+}
+
+function process () {
+   if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+         alert("Ca marche : le serveur a repondu !");
+         data = JSON.parse(xhr.responseText);
+
+         fill_db();
+
+         onClickSuivant(); //Initialisation de la liste des pays
+
+         //Ajout de l'écouteur sur le bouton suivant
+         let bt_suivant = document.getElementById("bt_suivant"); //Récupération de l'élément
+         bt_suivant.addEventListener("click",onClickSuivant); //Ajout de l'écouteur
+      } else {
+         alert("Erreur retour requete XML HTTP : "+xhr.status);
+      }
+   }
+}
+
+
+/*
 var data = 
 [
    {
@@ -20508,4 +20540,4 @@ var data =
       "independent": true
    }
 ];
-
+*/
